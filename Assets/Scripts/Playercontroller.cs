5bcafbdd87d3ebac538b5d93bd58ab89c6c2fecb
Martinez Tovar Edgar Ulises <170940@upslp.edu.mx>
2023-02-28 00:14:30 -0600

    v1

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
+    public float speed = 10.0f;
+    //rigidbody 2d  component
+    private Rigidbody2D playerRb;
+
+    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody2D>();
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //move the player with a d 
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //move the player
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+
+        //keep the player in bounds
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+
+        //jump  
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode2D.Impulse);
+        }
+       
     }
 }
